9873562fdab9e80ae01e32e3cbe143851121e21c
tecchu11 <agdjmp.ajtt@gmail.com>
2023-03-05 01:20:13 +0900

    Add http4k simple api

--- app/src/main/kotlin/com/example/tecchu11/app/App.kt
+++ app/src/main/kotlin/com/example/tecchu11/app/App.kt
@@ -1,5 +1,22 @@
 package com.example.tecchu11.app
 
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.server.Undertow
+import org.http4k.server.asServer
+
+private val port: String = System.getenv("APP_PORT") ?: "9000"
+
 fun main() {
-    println("Hello")
+    val app = { request: Request ->
+        Response(Status.OK).body(
+            """
+                {
+                "message": "Hello, ${request.query("name")}!"
+                }
+            """.trimIndent()
+        )
+    }
+    app.asServer(Undertow(port.toInt())).start()
 }
